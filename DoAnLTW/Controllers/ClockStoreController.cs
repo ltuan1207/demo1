aeaa8f5f446850d1b78cfacdb8bdc92241f8f79c
luutu <luutu@LAPTOP-R7T28BEC>
2023-03-05 21:09:12 +0700

    Add project files.

---
+++ DoAnLTW/Controllers/ClockStoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAnLTW.Controllers
+{
+    public class ClockStoreController : Controller
+    {
+        // GET: ClockStore
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
